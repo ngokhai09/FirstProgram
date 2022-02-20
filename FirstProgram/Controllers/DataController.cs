b14dba9391260b3f9e9d5ac0901ae770040a7a36
Khai Ngo Van <khaingovan2001@gmail.com>
2022-02-20 15:41:24 +0700

    Add project files.

---
+++ FirstProgram/Controllers/DataController.cs
@@ -0,0 +1,28 @@
+﻿using FirstProgram.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc; 
+
+namespace FirstProgram.Controllers
+{
+    public class DataController : Controller
+    {
+        /*static IList<Mess> MessList = new List<Mess>{
+                new Mess() { Message = "Message from Model 1" } ,
+                new Mess() { Message = "Message from Model 2" }                
+            };*/
+        // GET: Data
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Message from ViewBag"; 
+            ViewBag.CurrentTime = DateTime.Now;
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ FirstProgram/Controllers/ProduceController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstProgram.Models;
+
+namespace FirstProgram.Controllers
+{
+    public class ProduceController : Controller
+    {
+        // GET: Produce
+        WebBanVaLiEntities2 db = new WebBanVaLiEntities2();
+        public ActionResult Index()
+        {
+            
+
+            return View();
+        }
+        public PartialViewResult ChuDePartial()
+        {
+            return PartialView(db.tLoaiSPs.ToList());
+        }
+        public ViewResult SanPhamTheoLoai(String MaLoai = "vali")
+        {
+            tLoaiSP lsp = db.tLoaiSPs.SingleOrDefault(n => n.MaLoai == MaLoai);
+            if (lsp == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            List<tDanhMucSP> lstSanPham = db.tDanhMucSPs.Where(n => n.MaLoai == MaLoai).OrderBy(n => n.TenSP).ToList();
+            if (lstSanPham.Count == 0)
+            {
+                ViewBag.lstSanPham = "No Produce";
+            }
+            ViewBag.lstSanPham = db.tDanhMucSPs.ToList();
+            return View(lstSanPham);
+        }
+        public ViewResult ChiTietSanPham(string MaSP)
+        {
+            tDanhMucSP sanpham = db.tDanhMucSPs.Single(n => n.MaSP == MaSP);
+            if (sanpham == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(sanpham);
+        }
+    }
+}
